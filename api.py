94a37c9f38aeb5416fd9799721dc2bf9ee4488bb
dineshk813057 <40352058+dineshk813057@users.noreply.github.com>
2018-06-28 21:58:35 +0530

    Add files via upload

---
+++ api.py
@@ -0,0 +1,13 @@
+#question1
+#access token is like a key for using API every user has a unique access token without this key he/she cannot access API 
+
+
+#question 2
+#facebook->31.13.65.36
+#google->216.58.217.110
+
+
+#question4
+#API is a part of library which defines how an application communicate with external code.
+# for example->Instagram has some API which we can use so that we do not have to re-write code and if try to delete someone's it's not going to happen as API's has some restriction and if we use library of instagram and try to delete someone's account it will be a problem.
+
